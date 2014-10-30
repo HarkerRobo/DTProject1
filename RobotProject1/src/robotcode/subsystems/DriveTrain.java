713a159007f33bdbad0a1465dc24f75207e71b61
DivijaB <18divijab@students.harker.org>
2014-10-30 16:48:55 -0700

    Ported the Talons

--- RobotProject1/src/robotcode/subsystems/DriveTrain.java
+++ RobotProject1/src/robotcode/subsystems/DriveTrain.java
@@ -5,6 +5,7 @@
  */
 package robotcode.subsystems;
 
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -14,6 +15,14 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class DriveTrain extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+private Talon talon1, talon2, talon3, talon4;
+
+public DriveTrain() {
+talon1 = new Talon(1);
+talon2 = new Talon(2);
+talon3 = new Talon(3);
+talon4 = new Talon(4);
+}
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
